35dd29ecf90cc969e92d2c16a7ecbe7484b13ec7
HippiHo <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>
2023-07-21 12:38:30 +0200

    adds footer with links

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,38 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import { Inter } from "next/font/google";
+import Link from "next/link";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Planner App',
-  description: 'An app to find available days.',
-}
+  title: "Planner App",
+  description: "An app to find available days.",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <footer className="text-white flex justify-center my-5">
+          <div>
+            <Link
+              href={{ pathname: "/about" }}
+              className="hover:text-highlight"
+            >
+              About this App
+            </Link>
+            <span className="mx-2"> | </ span>
+            <Link href={{ pathname: "/" }} className="hover:text-reddish">
+              Refresh and back to the start
+            </Link>
+          </div>
+        </footer>
+      </body>
     </html>
-  )
+  );
 }
